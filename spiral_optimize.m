3019f15133e6bd7871400de99ff6f04b196fc662
chr <scut_chr@163.com>
2020-12-14 21:39:00 +0800

    add cubic spiral result

--- spiral_optimize.m
+++ spiral_optimize.m
@@ -1,7 +1,7 @@
 function p = spiral_optimize(lower_bound,upper_bound,start,target)   
     p0 = [start(4);target(4);norm([target(1)-start(1),target(2)-start(2)])];
 
-    lower_bounds = [-lower_bound;-lower_bound;p0(3)];
+    lower_bounds = [lower_bound;lower_bound;p0(3)];
     upper_bounds = [upper_bound;upper_bound;inf];
     min_objective = @(x) cubic_spiral_objective(x,start,target);
     
